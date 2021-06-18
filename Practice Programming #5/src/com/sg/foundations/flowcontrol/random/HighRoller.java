ed1b10be333e8f06f4efffe71fa8ceb1d849eeea
diegooviedo <diego.oviedo.sa@gmail.com>
2021-06-18 11:38:49 -0400

    adding HighRoller practice result

--- Practice Programming #5/src/com/sg/foundations/flowcontrol/random/HighRoller.java
+++ Practice Programming #5/src/com/sg/foundations/flowcontrol/random/HighRoller.java
@@ -1,20 +1,33 @@
 package com.sg.foundations.flowcontrol.random;
-import java.util.Random;
+import java.util.*;
 
 public class HighRoller {
 
     public static void main(String[] args) {
 
         Random diceRoller = new Random();
+        
+        int userInput = 0;//declaring and initializing the user's input as variable
+        
+        
+        System.out.println("Hey there!\n let's play a game...\n Chose the number of sides a dice can have, then lest roll your fortune...");
+        
+        userInput =new Scanner(System.in).nextInt(); //storing user's entry
+        
 
-        int rollResult = diceRoller.nextInt(6) + 1;
+        int rollResult = diceRoller.nextInt(userInput) + 1;
 
         System.out.println("TIME TO ROOOOOOLL THE DICE!");
         System.out.println("I rolled a " + rollResult);
 
         if (rollResult == 1) {
-            System.out.println("You rolled a critical failure!");
+            System.out.println("You rolled a critical failure! Ouch.");//changing result of condition
+        }if (rollResult == userInput) {//adding condition
+        	
+        	System.out.println("You rolled a critical! Nice job!");//adding result of condition
         }
+        
+        
+        
     }
 }
-
