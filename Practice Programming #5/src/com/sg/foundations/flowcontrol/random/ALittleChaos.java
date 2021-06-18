e00f3eb93b52de59af4b62a3a94e232b721db697
Diego <diego.oviedo.sa@gmail.com>
2021-06-17 21:47:51 -0400

    ALittleChaos commit

--- Practice Programming #5/src/com/sg/foundations/flowcontrol/random/ALittleChaos.java
+++ Practice Programming #5/src/com/sg/foundations/flowcontrol/random/ALittleChaos.java
@@ -11,7 +11,7 @@ public class ALittleChaos {
 	        System.out.println("Or a double: " + randomizer.nextDouble());
 	        System.out.println("Or even a boolean: " + randomizer.nextBoolean());
 
-	        int num = randomizer.nextInt(100);
+	        long num = randomizer.nextLong();// getting a random long 
 
 	        System.out.println("You can store a randomized result: " + num);
 	        System.out.println("And use it over and over again: " + num + ", " + num);
@@ -19,10 +19,9 @@ public class ALittleChaos {
 	        System.out.println("Or just keep generating new values");
 	        System.out.println("Here's a bunch of numbers from 0 - 100: ");
 
-	        System.out.print(randomizer.nextInt(101) + ", ");
-	        System.out.print(randomizer.nextInt(101) + ", ");
-	        System.out.print(randomizer.nextInt(101) + ", ");
-	        System.out.print(randomizer.nextInt(101) + ", ");
+	        System.out.println((randomizer.nextInt(51) + 50) + ", ");// This change will provide an random number between 0 and 50 and add 50
+	        System.out.println("This is the result of a random number splited by 4: " +(randomizer.nextInt(101) /4 ));//adding the random number into a math statement
+	        System.out.println("This is the result of asking a double random number: " +randomizer.nextDouble() + ",vs a random float: " + randomizer.nextFloat());
 	        System.out.print(randomizer.nextInt(101) + ", ");
 	        System.out.println(randomizer.nextInt(101));
 
