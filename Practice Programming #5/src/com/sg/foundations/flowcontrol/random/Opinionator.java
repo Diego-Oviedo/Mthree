b6728938c76f8b4a891c47fbc154b43c5e3a5815
diegooviedo <diego.oviedo.sa@gmail.com>
2021-06-18 12:23:17 -0400

    Adding Opinionator solution

--- Practice Programming #5/src/com/sg/foundations/flowcontrol/random/Opinionator.java
+++ Practice Programming #5/src/com/sg/foundations/flowcontrol/random/Opinionator.java
@@ -9,7 +9,7 @@ public class Opinionator {
         System.out.println("I can't decide what animal I like the best.");
         System.out.println("I know! Random can decide FOR ME!");
 
-        int x = randomizer.nextInt(5);
+        int x = randomizer.nextInt(5+1);// solution to get the CASE 5, adding the a next element 
 
         System.out.println("The number we chose was: " + x);
 
@@ -29,7 +29,8 @@ public class Opinionator {
             case 4:
                 System.out.println("Cockatoos are just so awesomme!");
                 break;
-            case 5:
+            case 5: // with the before code it would have never get to this case 
+            		//as it was set to provide a random number between 0(which is the starting point for java) and 4 which it is the 5th space for Java 
                 System.out.println("Have you ever met a naked mole-rat? They're GREAT!");
                 break;
         }
