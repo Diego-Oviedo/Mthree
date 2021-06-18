f80feddd6cfbd0b277dfbc5a6f25e6131b8bdf2e
Diego <diego.oviedo.sa@gmail.com>
2021-06-18 14:55:59 -0400

    Adding the first task for the Stay Positive practice...

--- Practice Programming #6/src/com/sg/foundations/flowcontrol/whiles/StayPositive.java
+++ Practice Programming #6/src/com/sg/foundations/flowcontrol/whiles/StayPositive.java
@@ -3,7 +3,21 @@ package com.sg.foundations.flowcontrol.whiles;
 public class StayPositive {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int counter = 10;
+		
+		System.out.println("Conting down...");
+		
+		while (counter > (-1) ) {
+		
+			System.out.println(counter);
+			
+			counter--;
+		}
+		
+		System.out.println("Blast off!!");
+		
+		
 
 	}
 
