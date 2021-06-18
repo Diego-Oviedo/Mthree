1a3368240fcbce02d8403b39e9119de990b8c23e
diegooviedo <diego.oviedo.sa@gmail.com>
2021-06-18 11:54:22 -0400

    Adding CoinFlipper practice results

--- Practice Programming #5/src/com/sg/foundations/flowcontrol/random/CoinFlipper.java
+++ Practice Programming #5/src/com/sg/foundations/flowcontrol/random/CoinFlipper.java
@@ -1,9 +1,27 @@
 package com.sg.foundations.flowcontrol.random;
 
+import java.util.*;
+
 public class CoinFlipper {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		 Random coinToss = new Random();
+		 
+		 boolean tails = coinToss.nextBoolean();
+		 
+		 System.out.println("Ready, Set, Flip....!!");
+		 
+		 if(tails) {
+			 
+			 System.out.println("You got TAILS!");
+			 
+		 }else {
+			 
+			 System.out.println("You got HEADS!");
+			 
+		 }
 
 	}
 
