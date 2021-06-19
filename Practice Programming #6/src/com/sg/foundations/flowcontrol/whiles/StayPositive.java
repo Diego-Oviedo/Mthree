0697510626ed66b4eb9bd9b3b048a946e8fb891b
Diego <diego.oviedo.sa@gmail.com>
2021-06-18 20:31:26 -0400

    Adding Stay Positive Result

--- Practice Programming #6/src/com/sg/foundations/flowcontrol/whiles/StayPositive.java
+++ Practice Programming #6/src/com/sg/foundations/flowcontrol/whiles/StayPositive.java
@@ -1,19 +1,37 @@
 package com.sg.foundations.flowcontrol.whiles;
+import java.util.*;
 
 public class StayPositive {
 
 	public static void main(String[] args) {
 		
-		int counter = 10;
+		System.out.println("Hello there!\n Give me a number an I'll give you th");
+		
+		int startPoint = new Scanner(System.in).nextInt();//Adding user´s input 
+		
+		int counter = 0 ;
 		
 		System.out.println("Conting down...");
 		
-		while (counter > (-1) ) {
 		
-			System.out.println(counter);
+		
+		while (startPoint > (-1) && counter < 10) {
+		
+			System.out.print(startPoint + " ");
 			
-			counter--;
+			startPoint--;
+			
+			counter ++;
+			
+			
+			if(startPoint > (-1) && counter == 10) { //if there still something to print and there is already 10 numbers in line, then break line
+				
+				System.out.print("\n");
+				
+				counter = 0;
+			}
 		}
+		
 		
 		System.out.println("Blast off!!");
 		
