2d1b574e1849306d31b5c7c0d43d6f59d3ea8f1e
diegooviedo <diego.oviedo.sa@gmail.com>
2021-06-19 14:15:01 -0400

    Adding WaitAwhile Result

--- Practice Programming #6/src/com/sg/foundations/flowcontrol/whiles/WaitAWhile.java
+++ Practice Programming #6/src/com/sg/foundations/flowcontrol/whiles/WaitAWhile.java
@@ -5,9 +5,9 @@ public class WaitAWhile {
     public static void main(String[] args) {
 
         int timeNow = 5;
-        int bedTime = 10;
+        int bedTime = 10;//if the time changes to 11, the loop will go a round further
 
-        while (timeNow < bedTime) {
+        while (timeNow < bedTime) {// as this condition means, if the
             System.out.println("It's only " + timeNow + " o'clock!");
             System.out.println("I think I'll stay up just a liiiiittle longer....");
             timeNow++; // Time passes
