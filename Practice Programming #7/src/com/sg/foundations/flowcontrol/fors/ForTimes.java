8b6fe2b0191a0bb56820891df48cc63326a80325
Diego <diego.oviedo.sa@gmail.com>
2021-06-20 12:07:47 -0400

    Adding ForTimes results

--- Practice Programming #7/src/com/sg/foundations/flowcontrol/fors/ForTimes.java
+++ Practice Programming #7/src/com/sg/foundations/flowcontrol/fors/ForTimes.java
@@ -1,9 +1,22 @@
 package com.sg.foundations.flowcontrol.fors;
+import java.util.*;
 
 public class ForTimes {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Hey there!\nLet know what table would you like me to solve for you, I´ll do it up to the 15th time...");
+		
+		int tableOf = new Scanner(System.in).nextInt();
+		
+		
+		
+		for(int i = 1 ; i < (15+1) ; i++) {
+			
+			
+			System.out.println(i + " * " + tableOf + " = " + (i * tableOf));
+		}
+		
 
 	}
 
