dc25aacd264399321c1603f416aada540e39ceb9
Diego <diego.oviedo.sa@gmail.com>
2021-06-20 17:41:49 -0400

    Adding modifications tot eh DifferentKettleOfFish sarter practice

--- Practice Programming #7/src/com/sg/foundations/flowcontrol/fors/DifferentKettleOfFish.java
+++ Practice Programming #7/src/com/sg/foundations/flowcontrol/fors/DifferentKettleOfFish.java
@@ -4,7 +4,7 @@ public class DifferentKettleOfFish {
     public static void main(String[] args) {
 
         int fish = 1;
-        while(fish < 10){
+        while(fish <= 10){// to get the expected output, the condition should be an equal or less, instead of an less than condition
             if(fish == 3){
                 System.out.println("RED fish!");
             }else if(fish == 4){
