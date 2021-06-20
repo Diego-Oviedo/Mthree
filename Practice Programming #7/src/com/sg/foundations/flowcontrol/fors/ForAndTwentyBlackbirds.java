47f5c23b38e2661b6c79de741380b9bfb322a6e8
diegooviedo <diego.oviedo.sa@gmail.com>
2021-06-20 11:37:07 -0400

    Adding ForAndTwentyBlackbirds Results

--- Practice Programming #7/src/com/sg/foundations/flowcontrol/fors/ForAndTwentyBlackbirds.java
+++ Practice Programming #7/src/com/sg/foundations/flowcontrol/fors/ForAndTwentyBlackbirds.java
@@ -4,7 +4,8 @@ public class ForAndTwentyBlackbirds {
 
 	public static void main(String[] args) {
         int birdsInPie = 0;
-        for (int i = 0; i < 20; i++) {
+        for (int i = 1; i <= 24; i++) { //change made so loop will stop once 24 birds counted...
+        		//iterator was increased to be 1, this way the print will start from 1 and not 0
             System.out.println("Blackbird #" + i + " goes into the pie!");
             birdsInPie++;
         }
