5a8168a0506a3728bf22ff08f3b2c0231095f43e
Diego <diego.oviedo.sa@gmail.com>
2021-06-20 18:30:45 -0400

    Adding ForByFor results

--- Practice Programming #7/src/com/sg/foundations/flowcontrol/fors/ForByFor.java
+++ Practice Programming #7/src/com/sg/foundations/flowcontrol/fors/ForByFor.java
@@ -2,9 +2,32 @@ package com.sg.foundations.flowcontrol.fors;
 
 public class ForByFor {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
 
-	}
+        for (int i = 0; i < 3; i++) {
+            System.out.print("|");
 
+            for (int j = 0; j < 3; j++) {
+                for (int k = 0; k < 3; k++) {
+                	// j moves columns
+                	// i moves rows 
+                	
+                	if(j == 1 && i == 0 || j == 1 && i == 2) {
+                		
+                		System.out.print("$");
+                	}else if(j == 1 && i == 1) {
+                		
+                		System.out.print("#");
+                	}else if(j == 0 && i == 1 || j == 2 && i == 1) {
+                		
+                		System.out.print("@");
+                	}else {                	
+                    System.out.print("*");
+                	}
+                }
+                System.out.print("|");
+            }
+            System.out.println("");
+        }
+    }
 }
