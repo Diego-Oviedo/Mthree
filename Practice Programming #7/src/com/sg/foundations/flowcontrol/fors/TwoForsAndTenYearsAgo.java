56324477fc087502c709df9815b2a6f3818eaba4
Diego <diego.oviedo.sa@gmail.com>
2021-06-20 17:33:32 -0400

    Adding TwoForsAndTenYearsAgo results

--- Practice Programming #7/src/com/sg/foundations/flowcontrol/fors/TwoForsAndTenYearsAgo.java
+++ Practice Programming #7/src/com/sg/foundations/flowcontrol/fors/TwoForsAndTenYearsAgo.java
@@ -8,13 +8,18 @@ public class TwoForsAndTenYearsAgo {
         System.out.print("What year would you like to count back from? ");
         int year = userInput.nextInt();
 
-        for (int i = 0; i <= 10; i++) {
+        for (int i = 0; i <= 10; i++) {//staring point is zero "i = 0"
+        							  //the end point is 10 "i <= 10"
             System.out.println(i + " years ago would be " + (year - i));
         }
 
         System.out.println("\nI can count backwards using a different way too...");
 
-        for (int i = year; i >= year - 10; i--) {
+        //both loops are clear for me, firs
+        
+        
+        for (int i = year; i >= year - 20; i--) {//staring point is the selected year "i = year"
+			  									//the end point is the selected year minus 10 "year - 10"
             System.out.println( (year - i) + " years ago would be " + i);
         }
     }
