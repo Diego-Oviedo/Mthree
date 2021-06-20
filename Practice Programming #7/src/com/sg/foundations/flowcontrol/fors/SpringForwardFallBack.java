4a958da6602b88267ff3e9c0f7b9f8e93c5c19b8
diegooviedo <diego.oviedo.sa@gmail.com>
2021-06-20 11:53:26 -0400

    Adding SpringForwardFallBack results

--- Practice Programming #7/src/com/sg/foundations/flowcontrol/fors/SpringForwardFallBack.java
+++ Practice Programming #7/src/com/sg/foundations/flowcontrol/fors/SpringForwardFallBack.java
@@ -4,12 +4,14 @@ public class SpringForwardFallBack {
 
     public static void main(String[] args) {
 
-        System.out.println("It's Spring...!");
-        for (int i = 0; i < 10; i++) {
+        System.out.println("It's Spring...!");//the initial start point for this loop is 0 and it loops up to 9
+        for (int i = 0; i < 11; i++) {//the ideal solution to match the range for the second loop would be 
+        								//changing the start point to 1 and the ending point to 11 (int i = 1; i < 11; i++)
+        	//However the request is to change only one of them, so I would increases the end point to 1, so it will count up to 10 
             System.out.print(i + ", ");
         }
 
-        System.out.println("\nOh no, it's fall...");
+        System.out.println("\nOh no, it's fall...");//the initial start point for this loop is 10 and it loops down up to 1
         for (int i = 10; i > 0; i--) {
             System.out.print(i + ", ");
         }
