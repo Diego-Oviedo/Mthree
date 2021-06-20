a9136da6b556b7475a7990cd26e2886357646671
Diego <diego.oviedo.sa@gmail.com>
2021-06-20 17:59:20 -0400

    Adding DifferentKettleOfFish results

--- Practice Programming #7/src/com/sg/foundations/flowcontrol/fors/DifferentKettleOfFish.java
+++ Practice Programming #7/src/com/sg/foundations/flowcontrol/fors/DifferentKettleOfFish.java
@@ -4,18 +4,34 @@ public class DifferentKettleOfFish {
     public static void main(String[] args) {
 
         int fish = 1;
-        while(fish <= 10){// to get the expected output, the condition should be an equal or less, instead of an less than condition
-            if(fish == 3){
-                System.out.println("RED fish!");
-            }else if(fish == 4){
-                System.out.println("BLUE fish!");
-            } else{
-                System.out.println(fish + " fish!");
-            }
+//        while(fish <= 10){// to get the expected output, the condition should be an equal or less, instead of an less than condition
+//            if(fish == 3){
+//                System.out.println("RED fish!");
+//            }else if(fish == 4){
+//                System.out.println("BLUE fish!");
+//            } else{
+//                System.out.println(fish + " fish!");
+//            }
+//
+//            fish++;
+//        }
 
-            fish++;
+        
+        for (int i = fish ; i <= 10 ; i++) {
+        	
+      	if(i == 3){
+              System.out.println("RED fish!");
+          }else if(i == 4){
+              System.out.println("BLUE fish!");
+          } else{
+              System.out.println(i + " fish!");
+          }
+        	
+        	
+        	
         }
-
+        
+        
     }
 }
 
