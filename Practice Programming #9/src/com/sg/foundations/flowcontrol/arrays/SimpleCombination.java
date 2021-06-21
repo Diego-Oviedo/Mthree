4d2ca1696c005d2a7822d295fb71ec7717f562d4
Diego <diego.oviedo.sa@gmail.com>
2021-06-20 22:53:08 -0400

    Adding SimpleCombination results

--- Practice Programming #9/src/com/sg/foundations/flowcontrol/arrays/SimpleCombination.java
+++ Practice Programming #9/src/com/sg/foundations/flowcontrol/arrays/SimpleCombination.java
@@ -4,12 +4,30 @@ public class SimpleCombination {
     public static void main(String[] args) {
         int[] firstHalf = {3, 7, 9, 10, 16, 19, 20, 34, 35, 45, 47, 49}; // 12 numbers
         int[] secondHalf = {51, 54, 68, 71, 75, 78, 82, 84, 85, 89, 91, 100}; // also 12!
-
+        
         int[] wholeNumbers = new int[24];
+        
+        int aux;
 
-        // Combining code should go here!
+        for (int i = 0 ; i <= 24 ; i++) {
+		
+        		if (i <= 11) {
+        			
+        			wholeNumbers [i] = firstHalf [i];
+        		
+        			
+        		}else if (i > 11 && i < 24) {
+        			
+        			wholeNumbers [i] = secondHalf [i - 12];
+        			
+        		}
+        		
+        }
 
-        // Printing code should go here
+        for (int n : wholeNumbers) {
+        	
+        	System.out.print(n + " ");
+        }
 
     }
 }
