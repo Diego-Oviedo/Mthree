cb80024f6721b01075cf2c009dcdc79aebc1f742
diegooviedo <diego.oviedo.sa@gmail.com>
2021-06-21 08:16:15 -0400

    Adding starting code for HiddenNuts practice

--- Practice Programming #9/src/com/sg/foundations/flowcontrol/arrays/HiddenNuts.java
+++ Practice Programming #9/src/com/sg/foundations/flowcontrol/arrays/HiddenNuts.java
@@ -1,10 +1,15 @@
 package com.sg.foundations.flowcontrol.arrays;
+import java.util.*;
 
 public class HiddenNuts {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
 
-	}
+        String[] hidingSpots = new String[100];
+        Random squirrel = new Random();
+        hidingSpots[squirrel.nextInt(hidingSpots.length)] = "Nut";
+        System.out.println("The nut has been hidden ...");
 
+        // Nut finding code should go here! 
+    }
 }
