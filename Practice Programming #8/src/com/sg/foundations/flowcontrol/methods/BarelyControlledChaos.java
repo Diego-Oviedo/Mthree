9909f0ada1679924d237484cf31054e6e5926ced
Diego <diego.oviedo.sa@gmail.com>
2021-06-20 22:02:00 -0400

    Adding BarelyControlledChaos results

--- Practice Programming #8/src/com/sg/foundations/flowcontrol/methods/BarelyControlledChaos.java
+++ Practice Programming #8/src/com/sg/foundations/flowcontrol/methods/BarelyControlledChaos.java
@@ -1,19 +1,20 @@
 package com.sg.foundations.flowcontrol.methods;
+import java.util.*;
 
 public class BarelyControlledChaos {
 
     public static void main(String[] args) {
 
-        ??? color = ???; // call color method here
-        ??? animal = ???; // call animal method again here
-        ??? colorAgain = ???; // call color method again here
-        ??? weight = ???; // call number method,
+        String color = getColor(); // call color method here
+        String animal = getAnimal(); // call animal method again here
+        String colorAgain = getColor(); // call color method again here
+        int weight = getRandomNum(5,200); // call number method,
             // with a range between 5 - 200
-        ??? distance = ???; // call number method,
+        int distance = getRandomNum(10,20); // call number method,
             // with a range between 10 - 20
-        ??? number = ???; // call number method,
+        int number = getRandomNum(10000,20000); // call number method,
             // with a range between 10000 - 20000
-        ??? time = ???; // call number method,
+        int time = getRandomNum(2,6); // call number method,
             // with a range between 2 - 6            
 
         System.out.println("Once, when I was very small...");
@@ -30,7 +31,63 @@ public class BarelyControlledChaos {
             + "let me tell you!");
     }
 
-    // ??? Method 1 ???
-    // ??? Method 2 ???
-    // ??? Method 3 ???
+    static public String getColor() {
+    	
+    	String color;
+    	
+    	int x = new Random().nextInt(4);
+    		if (x == 0) {
+    			color = "Green";
+    		}else if (x == 1) {
+    			color = "Blue";
+    		}else if (x == 2) {
+    			color = "Brown";
+    		}else if (x == 3) {
+    			color = "Red";
+    		}else if (x == 4) { 
+    			color = "Yellow";
+    		}else {
+    			color = "Orange";
+    		}
+    	
+    		return color;
+    		
+    }
+    
+    static public String getAnimal() {
+    	
+    	String animal;
+    	
+    	int x = new Random().nextInt(4);
+    		if (x == 0) {
+    			animal = "Shark";
+    		}else if (x == 1) {
+    			animal = "Bear";
+    		}else if (x == 2) {
+    			animal = "Turkey";
+    		}else if (x == 3) {
+    			animal = "Jellyfish";
+    		}else if (x == 4) { 
+    			animal = "Zebra";
+    		}else {
+    			animal = "Lion";
+    		}
+    	
+    		return animal;		
+    }
+    
+    
+    static public int getRandomNum(int min,int max) {
+    	
+    	int result = new Random().nextInt((max -  min) + min );
+    	
+    	if (result == 0) {
+    		result = 1;
+    	}else {
+    		result = result; 
+    	}
+    	
+    	return result;
+
+    }
 }
