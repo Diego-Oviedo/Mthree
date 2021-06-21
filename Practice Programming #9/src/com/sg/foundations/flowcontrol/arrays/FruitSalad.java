81f304eac5eafd3d75a711b92be29231a2e6eb40
Diego <diego.oviedo.sa@gmail.com>
2021-06-20 22:07:58 -0400

    Pushing project and its classes for practice 9

---
+++ Practice Programming #9/src/com/sg/foundations/flowcontrol/arrays/FruitSalad.java
@@ -0,0 +1,10 @@
+package com.sg.foundations.flowcontrol.arrays;
+
+public class FruitSalad {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Practice Programming #9/src/com/sg/foundations/flowcontrol/arrays/FruitsBasket.java
@@ -0,0 +1,10 @@
+package com.sg.foundations.flowcontrol.arrays;
+
+public class FruitsBasket {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
