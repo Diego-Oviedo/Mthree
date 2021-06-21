688b1ca4e2c21d6e9598c864f7ca8c55aef4133a
Diego <diego.oviedo.sa@gmail.com>
2021-06-20 22:36:15 -0400

    Adding starting code for SimpleCombination

--- Practice Programming #9/src/com/sg/foundations/flowcontrol/arrays/SimpleCombination.java
+++ Practice Programming #9/src/com/sg/foundations/flowcontrol/arrays/SimpleCombination.java
@@ -1,10 +1,15 @@
 package com.sg.foundations.flowcontrol.arrays;
 
 public class SimpleCombination {
+    public static void main(String[] args) {
+        int[] firstHalf = {3, 7, 9, 10, 16, 19, 20, 34, 35, 45, 47, 49}; // 12 numbers
+        int[] secondHalf = {51, 54, 68, 71, 75, 78, 82, 84, 85, 89, 91, 100}; // also 12!
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        int[] wholeNumbers = new int[24];
 
-	}
+        // Combining code should go here!
 
+        // Printing code should go here
+
+    }
 }
