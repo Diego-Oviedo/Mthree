82c66c28bd1e8bb2c3689c355cc0e6fe965c06e4
Diego <diego.oviedo.sa@gmail.com>
2021-06-20 23:11:45 -0400

    Adding startig code for FruitSalad practice

--- Practice Programming #9/src/com/sg/foundations/flowcontrol/arrays/FruitSalad.java
+++ Practice Programming #9/src/com/sg/foundations/flowcontrol/arrays/FruitSalad.java
@@ -2,9 +2,12 @@ package com.sg.foundations.flowcontrol.arrays;
 
 public class FruitSalad {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        String[] fruit = {"Kiwi Fruit", "Gala Apple", "Granny Smith Apple", "Cherry Tomato", "Gooseberry", "Beefsteak Tomato", "Braeburn Apple", "Blueberry", "Strawberry", "Navel Orange", "Pink Pearl Apple",  "Raspberry", "Blood Orange", "Sungold Tomato", "Fuji Apple", "Blackberry", "Banana", "Pineapple", "Florida Orange", "Kiku Apple", "Mango", "Satsuma Orange", "Watermelon", "Snozzberry"};
 
-	}
+        String[] fruitSalad;
 
+        // Code Recipe for fruit salad should go here!
+
+    }
 }
