0d83d1eb4b02ff53527dd5b66e9d716fe7b070c6
diegooviedo <diego.oviedo.sa@gmail.com>
2021-06-22 08:51:48 -0400

    Adding SummativeSums results

--- Java Basics/Assessment [Basic Programming Concepts]/src/com/diego/assessments/SummativeSums.java
+++ Java Basics/Assessment [Basic Programming Concepts]/src/com/diego/assessments/SummativeSums.java
@@ -3,8 +3,37 @@ package com.diego.assessments;
 public class SummativeSums {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("************************\nSUMMATIVE SUMS\n************************\n");
+		
+		int [] array_1 = { 1, 90, -33, -55, 67, -16, 28, -55, 15 };
+		
+		int [] array_2 = { 999, -60, -77, 14, 160, 301 };
+		
+		int [] array_3 = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, -99 };
+		
+		System.out.println("#1 Array Sum: " + sumOfArray(array_1));
+		
+		System.out.println("#2 Array Sum: " + sumOfArray(array_2));
+		
+		System.out.println("#3 Array Sum: " + sumOfArray(array_3));
+		
 
 	}
+	
+	
+	public static int sumOfArray(int [] array) {
+		
+		int result = 0 ;
+		
+		
+		for(int n : array) {
+			
+			result = result + n;
+		}
+		
+		return result;
+	}
+	
 
 }
