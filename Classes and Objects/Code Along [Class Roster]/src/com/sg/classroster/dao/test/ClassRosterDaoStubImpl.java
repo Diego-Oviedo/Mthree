9df066b7f67d4c970de1bde6047aa0e5cc26dc63
diegooviedo <diego.oviedo.sa@gmail.com>
2021-06-30 10:37:58 -0400

    Adding statful unit testing practices to the Class Roster project

---
+++ Classes and Objects/Code Along [Class Roster]/src/com/sg/classroster/dao/test/ClassRosterDaoStubImpl.java
@@ -0,0 +1,58 @@
+package com.sg.classroster.dao.test;
+import com.sg.classroster.dao.*;
+import com.sg.classroster.dto.*;
+import java.util.*;
+
+public class ClassRosterDaoStubImpl implements ClassRosterDAO {
+
+    public Student onlyStudent;
+
+    public ClassRosterDaoStubImpl() {
+        onlyStudent = new Student("0001");
+        onlyStudent.setFirstName("Ada");
+        onlyStudent.setLastName("Lovelace");
+        onlyStudent.setCohort("Java-May-1845");
+    }
+
+    public ClassRosterDaoStubImpl(Student testStudent){
+         this.onlyStudent = testStudent;
+     }
+
+    @Override
+    public Student addStudent(String studentId, Student student)
+                  throws ClassRosterPersistenceException {
+        if (studentId.equals(onlyStudent.getStudentId())) {
+            return onlyStudent;
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public List<Student> getAllStudents()
+                 throws ClassRosterPersistenceException {
+        List<Student> studentList = new ArrayList<>();
+        studentList.add(onlyStudent);
+        return studentList;
+    }
+
+    @Override
+    public Student getStudent(String studentId)
+                throws ClassRosterPersistenceException {
+        if (studentId.equals(onlyStudent.getStudentId())) {
+            return onlyStudent;
+        } else {
+            return null;
+        }       
+    }
+
+    @Override
+    public Student removeStudent(String studentId)
+                throws ClassRosterPersistenceException {
+        if (studentId.equals(onlyStudent.getStudentId())) {
+            return onlyStudent;
+        } else {
+            return null;
+        }
+    }   
+}
