d8a3e30c7dce2838b9c2ab6d06e46321144b3b4c
diegooviedo <diego.oviedo.sa@gmail.com>
2021-07-01 13:32:22 -0400

    Adding Exercise: DVD Library Lambdas and Streams Results

---
+++ Intermediate Java/Exercise [DVD Library Lambdas and Streams]/src/com/diego/DVDLibrary/dao/DVDLibraryDAOImpl.java
@@ -0,0 +1,281 @@
+	package com.diego.DVDLibrary.dao;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import com.diego.DVDLibrary.dto.DVD;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
+
+public class DVDLibraryDAOImpl implements DVDLibraryDAO {
+	
+	public final String FILE_PATH;
+	
+
+	public static final String DELIMITER = "::";
+	
+	private Map<String, DVD> DVDs = new HashMap<>();
+	
+
+	public DVDLibraryDAOImpl() {
+		super();
+		FILE_PATH = "DVDLibrary_data.txt";
+	}
+	
+	public DVDLibraryDAOImpl(String fILE_PATH) {
+		super();
+		FILE_PATH = fILE_PATH;
+	}
+	
+
+	@Override
+	public DVD addDVD(String SKU, DVD dvd) throws DVDLibraryExceptionDAO {
+		DVD DVD = DVDs.put(SKU, dvd);
+		writeDVD();
+		return DVD;
+	}
+
+	@Override
+	public DVD removeDVD(String SKU) throws DVDLibraryExceptionDAO {
+		loadData();
+		DVD DVD = DVDs.remove(SKU);
+		writeDVD();
+		return DVD;
+	}
+
+	@Override
+	public int retreiveNumDVDs() throws DVDLibraryExceptionDAO {
+		retreiveAllDVDs();
+		return DVDs.size();
+	}
+
+	@Override
+	public List<DVD> retreiveAllDVDs() throws DVDLibraryExceptionDAO {
+		loadData();
+		return new ArrayList(DVDs.values());
+	}
+
+	@Override
+	public DVD findDVDByTitle(String title) throws DVDLibraryExceptionDAO {
+		loadData();
+		
+		DVD DVD = null;
+		
+		List<DVD> DVDs = retreiveAllDVDs();
+		 
+		for (DVD current_DVD : DVDs) {
+			
+			if(current_DVD.getTitle().equalsIgnoreCase(title)) {
+				DVD = current_DVD;
+			}
+
+		}
+
+		return DVD;
+	}
+
+	@Override
+	public DVD updateDVD(String SKU, DVD dvd) throws DVDLibraryExceptionDAO {
+		DVD DVD_deleted = removeDVD(SKU);
+		
+		String new_SKU = (dvd.getTitle().substring(0, 2).toUpperCase() + dvd.getStudio().substring(0, 2).toUpperCase() + 00 + "" + dvd.getRelease_date().getYear());
+		
+		dvd.setSKU(new_SKU);
+		
+		DVD DVD_updated = addDVD(dvd.getSKU(), dvd);
+		
+		return DVD_updated;
+	}
+	
+	
+	//UTILITY METHODS 
+
+	private void loadData() throws DVDLibraryExceptionDAO{
+		   
+		Scanner reader;
+		 
+	    try {
+	        
+	    	File file = new File(FILE_PATH);
+		      reader = new Scanner(file);
+		      reader.useDelimiter(DELIMITER);
+	    } catch (FileNotFoundException e) {
+	        throw new DVDLibraryExceptionDAO(
+	                "Could not load data into memory.", e);
+	    }
+	    
+	    String currentLine;
+	    String SKU;
+	    
+	    DVD currentDVD;
+	    	    
+	    while (reader.hasNextLine()) {//while there is data to persist
+	        
+	        currentLine = reader.nextLine();
+
+	        currentDVD = unmarshallObject(currentLine); //Convert a line into an object 
+	        
+	        SKU = (currentDVD.getTitle().substring(0, 2).toUpperCase() + currentDVD.getStudio().substring(0, 2).toUpperCase() + 00 + "" + currentDVD.getRelease_date().getYear());
+	        
+	        currentDVD.setSKU(SKU);
+	        
+	        DVDs.put(currentDVD.getSKU(), currentDVD);//put the object into the HashMap 
+
+	    }
+	    
+	    reader.close();//once done, close the reader
+	}
+	
+	
+	private DVD unmarshallObject(String objectAsText){
+		
+		String[] objectTokens = objectAsText.split(DELIMITER);//the split method will return an array of string  with every piece of data in each element 
+		
+		 
+		String SKU = (objectTokens[0].substring(0, 2).toUpperCase() + objectTokens[4].substring(0, 2).toUpperCase() + 00 + "" + objectTokens[1]);
+        
+		DVD DVD = new DVD(SKU);
+		
+		DVD.setTitle(objectTokens[0]);
+		
+		DVD.setRelease_date(DVD.yearToDateFormatter(objectTokens[1]));
+
+		DVD.setMPAA_rating(Double.valueOf(objectTokens[2]));
+		
+		DVD.setAuthor(objectTokens[3]);
+		
+		DVD.setStudio(objectTokens[4]);
+		
+		DVD.setUser_note(objectTokens[5]);
+		
+	    return DVD;
+	}
+	
+	private String marshallObject(DVD DVD){
+		String DVDAsText = DVD.getTitle() + DELIMITER; 
+		DVDAsText += DVD.getRelease_date().getYear()+ DELIMITER;
+		DVDAsText += DVD.getMPAA_rating() + DELIMITER;
+		DVDAsText += DVD.getAuthor() + DELIMITER;
+		DVDAsText += DVD.getStudio() + DELIMITER;
+		DVDAsText += DVD.getUser_note();
+		
+		return DVDAsText;
+	}
+	
+	private void writeDVD() throws DVDLibraryExceptionDAO { 
+		PrintWriter out;
+		
+		try {
+	        out = new PrintWriter(new FileWriter(FILE_PATH));
+	    } catch (IOException e) {
+	        throw new DVDLibraryExceptionDAO(
+	                "Could not save student data.", e);
+	    }
+		
+		String DVDAsText;
+	    List<DVD> DVDs = this.retreiveAllDVDs();
+	    for (DVD DVD : DVDs) {
+	        
+	    	DVDAsText = marshallObject(DVD);
+	        // write the object to the file
+	        out.println(DVDAsText);
+	        // force PrintWriter to write line to the file
+	        out.flush();
+	    }
+	    // Clean up
+	    out.close();
+		
+	}
+
+	@Override
+	public List<DVD> retreiveDVDsFromTheLastGivenYears(int numberOfYearsBack) throws DVDLibraryExceptionDAO {
+		List<DVD> dvds = retreiveAllDVDs();
+		
+		LocalDate date_calculated = LocalDate.now();
+		
+		int year = date_calculated.minusYears(numberOfYearsBack).getYear();
+		
+		return dvds.stream().filter((dvd) -> dvd.getRelease_date().getYear() >= year).collect(Collectors.toList());
+	}
+
+	@Override
+	public List<DVD> retreiveDVDsbyMPAARating(double rating) throws DVDLibraryExceptionDAO {
+		List<DVD> dvds = retreiveAllDVDs();
+		
+		return dvds.stream().filter((dvd) -> dvd.getMPAA_rating() == rating).collect(Collectors.toList());
+	}
+
+	@Override
+	public List<DVD> retreiveDVDsbyDirector(String director) throws DVDLibraryExceptionDAO {
+		List<DVD> dvds = retreiveAllDVDs();
+		
+		return dvds.stream().filter((dvd) -> dvd.getAuthor().toLowerCase().contains(director.toLowerCase())).collect(Collectors.toList());
+	}
+
+	@Override
+	public List<DVD> retreiveDVDsbyStudio(String studio) throws DVDLibraryExceptionDAO {
+		List<DVD> dvds = retreiveAllDVDs();
+		
+		return dvds.stream().filter((dvd) -> dvd.getStudio().toLowerCase().contains(studio.toLowerCase())).collect(Collectors.toList());
+	}
+
+	@Override
+	public int getDVDsAverageOfYear() throws DVDLibraryExceptionDAO {
+		List<DVD> dvds = retreiveAllDVDs();
+		
+		OptionalDouble average = dvds.stream().mapToInt((dvd) -> dvd.getRelease_date().getYear()).average();
+		
+		String average_string = String.valueOf(average)+"";
+		
+		average_string = average_string.substring(15,19);
+		
+		System.out.println("Sting value of average_string : " + average_string);
+		
+		int actual_average = Integer.valueOf(average_string);
+		
+		return actual_average;
+	}
+
+	@Override
+	public DVD findNewestDVD() throws DVDLibraryExceptionDAO {
+		List<DVD> dvds = retreiveAllDVDs();
+		
+		List<DVD> result = dvds.stream().sorted((o1, o2)->  String.valueOf(o1.getRelease_date().getYear()).
+                compareTo(String.valueOf(o1.getRelease_date().getYear()))).
+                collect(Collectors.toList());
+		
+		return result.get((result.size()-1));
+	}
+
+	@Override
+	public DVD findOldestDVD() throws DVDLibraryExceptionDAO {
+		List<DVD> dvds = retreiveAllDVDs();
+		
+		List<DVD> result = dvds.stream().sorted((o1, o2)->  String.valueOf(o1.getRelease_date().getYear()).
+                compareTo(String.valueOf(o1.getRelease_date().getYear()))).
+                collect(Collectors.toList());
+		
+		return result.get(0);
+	}
+
+	@Override
+	public int getDVDsAverageNotesPerMovie() throws DVDLibraryExceptionDAO {
+		List<DVD> dvds = retreiveAllDVDs();
+		
+		int dvds_w_notes =dvds.stream()
+        		.filter((dvd) -> dvd.getUser_note() != null ).collect(Collectors.toList()).size();
+		System.out.println(dvds_w_notes);
+		
+		int average = dvds_w_notes / dvds.size();
+		
+		return average;
+	}
+
+}
---
+++ Intermediate Java/Exercise [DVD Library Lambdas and Streams]/src/com/diego/DVDLibrary/dto/DVD.java
@@ -0,0 +1,161 @@
+package com.diego.DVDLibrary.dto;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
+
+public class DVD {
+	
+	private String SKU;
+	private String title;
+	private LocalDate release_date;
+	private double MPAA_rating;
+	private String author;
+	private String studio;
+	private String user_note;
+	
+	public DVD(String sKU) {
+		super();
+		SKU = sKU;
+	}
+	public DVD() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public String getSKU() {
+		return SKU;
+	}
+
+	public void setSKU(String sKU) {
+		SKU = sKU;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public LocalDate getRelease_date() {
+		return release_date;
+	}
+
+	public void setRelease_date(LocalDate release_date) {
+		this.release_date = release_date;
+	}
+
+	public double getMPAA_rating() {
+		return MPAA_rating;
+	}
+
+	public void setMPAA_rating(double mPAA_rating) {
+		MPAA_rating = mPAA_rating;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getStudio() {
+		return studio;
+	}
+
+	public void setStudio(String studio) {
+		this.studio = studio;
+	}
+
+	public String getUser_note() {
+		return user_note;
+	}
+
+	public void setUser_note(String user_note) {
+		this.user_note = user_note;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(MPAA_rating);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((SKU == null) ? 0 : SKU.hashCode());
+		result = prime * result + ((author == null) ? 0 : author.hashCode());
+		result = prime * result + ((release_date == null) ? 0 : release_date.hashCode());
+		result = prime * result + ((studio == null) ? 0 : studio.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		result = prime * result + ((user_note == null) ? 0 : user_note.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DVD other = (DVD) obj;
+		if (Double.doubleToLongBits(MPAA_rating) != Double.doubleToLongBits(other.MPAA_rating))
+			return false;
+		if (SKU == null) {
+			if (other.SKU != null)
+				return false;
+		} else if (!SKU.equals(other.SKU))
+			return false;
+		if (author == null) {
+			if (other.author != null)
+				return false;
+		} else if (!author.equals(other.author))
+			return false;
+		if (release_date == null) {
+			if (other.release_date != null)
+				return false;
+		} else if (!release_date.equals(other.release_date))
+			return false;
+		if (studio == null) {
+			if (other.studio != null)
+				return false;
+		} else if (!studio.equals(other.studio))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		if (user_note == null) {
+			if (other.user_note != null)
+				return false;
+		} else if (!user_note.equals(other.user_note))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "DVD [SKU=" + SKU + ", title=" + title + ", release_date=" + release_date + ", MPAA_rating="
+				+ MPAA_rating + ", author=" + author + ", studio=" + studio + ", user_note=" + user_note + "]";
+	}
+	
+	//UTILITY METHODS 
+	
+		public static LocalDate yearToDateFormatter(String year) {
+			
+			
+			String date_to_format = "01/01/"+year+"";
+			
+		
+			LocalDate date_formatted = LocalDate.parse(date_to_format, DateTimeFormatter.ofPattern("MM/dd/yyyy"));
+			
+			return date_formatted;
+		}
+	
+	
+}
