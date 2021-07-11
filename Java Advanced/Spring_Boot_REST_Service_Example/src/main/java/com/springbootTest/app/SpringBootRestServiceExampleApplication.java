1a57c108f22a686e8e57ba7944cb976182570f17
diegooviedo <diego.oviedo.sa@gmail.com>
2021-07-10 21:39:42 -0400

    Pushing REST and Spring JDBC practices to the HUB

---
+++ Java Advanced/Spring_Boot_REST_Service_Example/src/main/java/com/springbootTest/app/SpringBootRestServiceExampleApplication.java
@@ -0,0 +1,13 @@
+package com.springbootTest.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestServiceExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestServiceExampleApplication.class, args);
+	}
+
+}
---
+++ Java Advanced/Spring_Boot_REST_Service_Example/src/test/java/com/springbootTest/app/SpringBootRestServiceExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootTest.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestServiceExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
