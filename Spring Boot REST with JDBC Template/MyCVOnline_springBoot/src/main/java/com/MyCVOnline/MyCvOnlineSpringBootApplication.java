bc3e1995cf9f732ebbe76003b91871f7f76c3bca
diegooviedo <diego.oviedo.sa@gmail.com>
2021-07-16 13:40:33 -0400

    Pushing MyCVOnline Project

---
+++ Spring Boot REST with JDBC Template/MyCVOnline_springBoot/src/main/java/com/MyCVOnline/MyCvOnlineSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.MyCVOnline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCvOnlineSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCvOnlineSpringBootApplication.class, args);
+	}
+
+}
---
+++ Spring Boot REST with JDBC Template/MyCVOnline_springBoot/src/test/java/com/MyCVOnline/MyCvOnlineSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MyCVOnline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCvOnlineSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
